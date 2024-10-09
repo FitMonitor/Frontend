670b7b34fcbe7618f09cdd5273df5c82ddb3f9e7
DiogoSilva1904 <diogompsilva2000@gmail.com>
2024-10-09 16:27:56 +0100

    backend project initiated

---
+++ backend/fitmonitor/src/test/java/deti/es/fitmonitor/FitmonitorApplicationTests.java
@@ -0,0 +1,13 @@
+package deti.es.fitmonitor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitmonitorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
