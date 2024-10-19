1aedfe3c41f12a1ebeed75f22565986a84c5c0b7
DiogoSilva1904 <diogompsilva2000@gmail.com>
2024-10-19 17:03:48 +0100

    environment delete

--- frontend/frontend/src/environments/environment.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-    production: false,
-    tokenUrl: 'https://identity-server.auth.eu-north-1.amazoncognito.com/oauth2/token',
-    redirectUri: 'http://localhost:4200/callback',
-    clientSecret: 'Basic NGRydnRndm12N2t2azQ2NWN0Y2t0cDZlNGY6MTYxNjZldGs3ZHQ2YjVvYjJnYmd1MjNrdGlvdjFjdXR0MHZnZ3NsanVtajBtaWRrbTEyYw==' // Base64 encoded client_id:client_secret
-  };
-  
